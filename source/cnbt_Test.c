e4c0d9b2b2da8d5041c811d88b0a78776f2a88bf
ceary <bxsxmi@163.com>
2024-01-04 23:16:34 +0800

    add test nbt file

--- source/cnbt_Test.c
+++ source/cnbt_Test.c
@@ -12,7 +12,8 @@
 
 int main(void)
 {
-	int fd = open("../data/chunk/q.nbt", O_RDONLY); 
+	//int fd = open("../data/chunk/q.nbt", O_RDONLY); 
+	int fd = open("../data/generator_t2.nbt", O_RDONLY); 
 	int len = lseek(fd, 0, SEEK_END);
 	chunk_data data = mmap(NULL, len, PROT_READ, MAP_FILE|MAP_PRIVATE, fd, 0);
 	if(data == MAP_FAILED)
@@ -34,22 +35,22 @@ int main(void)
 
 	printNbt(this_nbt, 0);
 
-	nbt childnbt = nbt_GetItemOfName(this_nbt, "Heightmaps");
-	nbt cchildnbt = nbt_GetItemOfName(childnbt, "OCEAN_FLOOR");
-	int size = nbt_GetArraySize(cchildnbt);
-	printf("%s %d\n", cchildnbt->name, nbt_GetArraySize(cchildnbt));
-	uint64_t value;
-	for(int i = 0; i < size; i++)
-	{
-		if(nbt_GetArrayItem(cchildnbt, i, &value) < 0)
-		{
-			printf("error\n");
-		}
-		printf("%lld ", value);
-	}
-	printf("\n");
-	nbt com = nbt_GetItemOfIndex(this_nbt, 8);
-	printf("%p\n%p\n", com, childnbt);
+	//nbt childnbt = nbt_GetItemOfName(this_nbt, "Heightmaps");
+	//nbt cchildnbt = nbt_GetItemOfName(childnbt, "OCEAN_FLOOR");
+	//int size = nbt_GetArraySize(cchildnbt);
+	//printf("%s %d\n", cchildnbt->name, nbt_GetArraySize(cchildnbt));
+	//uint64_t value;
+	//for(int i = 0; i < size; i++)
+	//{
+	//	if(nbt_GetArrayItem(cchildnbt, i, &value) < 0)
+	//	{
+	//		printf("error\n");
+	//	}
+	//	printf("%lld ", value);
+	//}
+	//printf("\n");
+	//nbt com = nbt_GetItemOfIndex(this_nbt, 8);
+	//printf("%p\n%p\n", com, childnbt);
 
 	return 0;
 }
